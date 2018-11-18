828d4f015bb6cc927e7807edb20625348be9a39c
kooshkestani <kooshkestani@outlook.com>
2018-11-19 00:17:13 +0330

    two_commit_dashboard_admin_$_user

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
