006910876d7608dc65b4b376ee505bbba1d58fa6
kooshkestani <kooshkestani@outlook.com>
2018-11-20 11:10:53 +0330

    add_Lang_fa_for_errors_renew

---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'این رکورد با اطلاعات ما مطابقت ندارد.',
+    'throttle' => 'تلاشهای ورود بسیار زیاد است لطفا :seconds seconds بعد دوباره امتحان کنید .',
+
+];
