14dad05983eb99e54260d24a1befcd3e8e9175e5
kooshkestani <kooshkestani@outlook.com>
2018-11-21 15:14:39 +0330

    create_model_ecommerc

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
 
         /*
          * Application Service Providers...
