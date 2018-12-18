a9d59b240325eaa9d7749561d18558b8e4a4726d
kooshkestani <kooshkestani@outlook.com>
2018-12-18 15:23:32 +0330

    checkout fixed

--- resources/views/Theme2/main/content/checkout-page.blade.php
+++ resources/views/Theme2/main/content/checkout-page.blade.php
@@ -46,176 +46,158 @@
                     <form class="card-body" method="post" action="{{route('checkout.store')}}">
                     {{csrf_field()}}
                     <!--Grid row-->
-                        <div class="row">
+                        {{--<div class="row">--}}
 
                             <!--Grid column-->
-                            <div class="col-md-6 mb-2">
+                            {{--<div class="col-md-6 mb-2">--}}
 
                                 <!--firstName-->
-                                <div class="md-form ">
-                                    <input type="text" id="firstName" class="form-control">
-                                    <label for="firstName" class="">First name</label>
+                                <div class="md-form mb-5">
+                                    <input type="text" id="firstName" class="form-control" name="username" value="{{$userInfo->name}}" placeholder="{{$userInfo->name}}">
+                                    <label for="firstName" class="">نام کاربر</label>
                                 </div>
 
-                            </div>
+                            {{--</div>--}}
                             <!--Grid column-->
 
                             <!--Grid column-->
-                            <div class="col-md-6 mb-2">
 
-                                <!--lastName-->
-                                <div class="md-form">
-                                    <input type="text" id="lastName" class="form-control">
-                                    <label for="lastName" class="">Last name</label>
-                                </div>
-
-                            </div>
-                            <!--Grid column-->
-
-                        </div>
+                        {{--</div>--}}
                         <!--Grid row-->
 
-                        <!--Username-->
-                        <div class="md-form input-group pl-0 mb-5">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1">@</span>
-                            </div>
-                            <input type="text" class="form-control py-0" placeholder="Username"
-                                   aria-describedby="basic-addon1">
-                        </div>
 
                         <!--email-->
                         <div class="md-form mb-5">
-                            <input type="text" id="email" class="form-control" placeholder="youremail@example.com">
-                            <label for="email" class="">Email (optional)</label>
+                            <input type="text" id="email" class="form-control" name="email"  value="{{$userInfo->email}}" placeholder="{{$userInfo->email}}">
+                            <label for="email" class="">ایمیل</label>
                         </div>
 
                         <!--address-->
                         <div class="md-form mb-5">
-                            <input type="text" id="address" class="form-control" placeholder="1234 Main St">
-                            <label for="address" class="">Address</label>
+                            <input type="text" id="address" class="form-control" name="address" value="{{$userInfo->address->addressline}}" placeholder="{{$userInfo->address->addressline}}">
+                            <label for="address" class="">آدرس</label>
                         </div>
 
-                        <!--address-2-->
+                        <!--telephone-->
                         <div class="md-form mb-5">
-                            <input type="text" id="address-2" class="form-control" placeholder="Apartment or suite">
-                            <label for="address-2" class="">Address 2 (optional)</label>
+                            <input type="text" id="address" class="form-control" name="phone" value="{{$userInfo->address->phone}}" placeholder="{{$userInfo->address->phone}}">
+                            <label for="address" class="">تلفن</label>
                         </div>
 
                         <!--Grid row-->
-                        <div class="row">
+                        {{--<div class="row">--}}
 
-                            <!--Grid column-->
-                            <div class="col-lg-4 col-md-12 mb-4">
+                            {{--<!--Grid column-->--}}
+                            {{--<div class="col-lg-4 col-md-12 mb-4">--}}
 
-                                <label for="country">Country</label>
-                                <select class="custom-select d-block w-100" id="country" required>
-                                    <option value="">Choose...</option>
-                                    <option>United States</option>
-                                </select>
-                                <div class="invalid-feedback">
-                                    Please select a valid country.
-                                </div>
+                                {{--<label for="country">Country</label>--}}
+                                {{--<select class="custom-select d-block w-100" id="country" required>--}}
+                                    {{--<option value="">Choose...</option>--}}
+                                    {{--<option>United States</option>--}}
+                                {{--</select>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Please select a valid country.--}}
+                                {{--</div>--}}
 
-                            </div>
-                            <!--Grid column-->
+                            {{--</div>--}}
+                            {{--<!--Grid column-->--}}
 
-                            <!--Grid column-->
-                            <div class="col-lg-4 col-md-6 mb-4">
+                            {{--<!--Grid column-->--}}
+                            {{--<div class="col-lg-4 col-md-6 mb-4">--}}
 
-                                <label for="state">State</label>
-                                <select class="custom-select d-block w-100" id="state" required>
-                                    <option value="">Choose...</option>
-                                    <option>California</option>
-                                </select>
-                                <div class="invalid-feedback">
-                                    Please provide a valid state.
-                                </div>
+                                {{--<label for="state">State</label>--}}
+                                {{--<select class="custom-select d-block w-100" id="state" required>--}}
+                                    {{--<option value="">Choose...</option>--}}
+                                    {{--<option>California</option>--}}
+                                {{--</select>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Please provide a valid state.--}}
+                                {{--</div>--}}
 
-                            </div>
-                            <!--Grid column-->
+                            {{--</div>--}}
+                            {{--<!--Grid column-->--}}
 
-                            <!--Grid column-->
-                            <div class="col-lg-4 col-md-6 mb-4">
+                            {{--<!--Grid column-->--}}
+                            {{--<div class="col-lg-4 col-md-6 mb-4">--}}
 
-                                <label for="zip">Zip</label>
-                                <input type="text" class="form-control" id="zip" placeholder="" required>
-                                <div class="invalid-feedback">
-                                    Zip code required.
-                                </div>
+                                {{--<label for="zip">Zip</label>--}}
+                                {{--<input type="text" class="form-control" id="zip" placeholder="" required>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Zip code required.--}}
+                                {{--</div>--}}
 
-                            </div>
-                            <!--Grid column-->
+                            {{--</div>--}}
+                            {{--<!--Grid column-->--}}
 
-                        </div>
+                        {{--</div>--}}
                         <!--Grid row-->
 
-                        <hr>
+                        {{--<hr>--}}
 
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="same-address">
-                            <label class="custom-control-label" for="same-address">Shipping address is the same as my
-                                billing address</label>
-                        </div>
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="save-info">
-                            <label class="custom-control-label" for="save-info">Save this information for next
-                                time</label>
-                        </div>
+                        {{--<div class="custom-control custom-checkbox">--}}
+                            {{--<input type="checkbox" class="custom-control-input" id="same-address">--}}
+                            {{--<label class="custom-control-label" for="same-address">Shipping address is the same as my--}}
+                                {{--billing address</label>--}}
+                        {{--</div>--}}
+                        {{--<div class="custom-control custom-checkbox">--}}
+                            {{--<input type="checkbox" class="custom-control-input" id="save-info">--}}
+                            {{--<label class="custom-control-label" for="save-info">Save this information for next--}}
+                                {{--time</label>--}}
+                        {{--</div>--}}
 
-                        <hr>
+                        {{--<hr>--}}
 
-                        <div class="d-block my-3">
-                            <div class="custom-control custom-radio">
-                                <input id="credit" name="paymentMethod" type="radio" class="custom-control-input"
-                                       checked required>
-                                <label class="custom-control-label" for="credit">Credit card</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="debit" name="paymentMethod" type="radio" class="custom-control-input"
-                                       required>
-                                <label class="custom-control-label" for="debit">Debit card</label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input"
-                                       required>
-                                <label class="custom-control-label" for="paypal">Paypal</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <label for="cc-name">Name on card</label>
-                                <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                                <small class="text-muted">Full name as displayed on card</small>
-                                <div class="invalid-feedback">
-                                    Name on card is required
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <label for="cc-number">Credit card number</label>
-                                <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                                <div class="invalid-feedback">
-                                    Credit card number is required
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3 mb-3">
-                                <label for="cc-expiration">Expiration</label>
-                                <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                                <div class="invalid-feedback">
-                                    Expiration date required
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-3">
-                                <label for="cc-expiration">CVV</label>
-                                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                                <div class="invalid-feedback">
-                                    Security code required
-                                </div>
-                            </div>
-                        </div>
-                        <hr class="mb-4">
+                        {{--<div class="d-block my-3">--}}
+                            {{--<div class="custom-control custom-radio">--}}
+                                {{--<input id="credit" name="paymentMethod" type="radio" class="custom-control-input"--}}
+                                       {{--checked required>--}}
+                                {{--<label class="custom-control-label" for="credit">Credit card</label>--}}
+                            {{--</div>--}}
+                            {{--<div class="custom-control custom-radio">--}}
+                                {{--<input id="debit" name="paymentMethod" type="radio" class="custom-control-input"--}}
+                                       {{--required>--}}
+                                {{--<label class="custom-control-label" for="debit">Debit card</label>--}}
+                            {{--</div>--}}
+                            {{--<div class="custom-control custom-radio">--}}
+                                {{--<input id="paypal" name="paymentMethod" type="radio" class="custom-control-input"--}}
+                                       {{--required>--}}
+                                {{--<label class="custom-control-label" for="paypal">Paypal</label>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<div class="row">--}}
+                            {{--<div class="col-md-6 mb-3">--}}
+                                {{--<label for="cc-name">Name on card</label>--}}
+                                {{--<input type="text" class="form-control" id="cc-name" placeholder="" required>--}}
+                                {{--<small class="text-muted">Full name as displayed on card</small>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Name on card is required--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-md-6 mb-3">--}}
+                                {{--<label for="cc-number">Credit card number</label>--}}
+                                {{--<input type="text" class="form-control" id="cc-number" placeholder="" required>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Credit card number is required--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<div class="row">--}}
+                            {{--<div class="col-md-3 mb-3">--}}
+                                {{--<label for="cc-expiration">Expiration</label>--}}
+                                {{--<input type="text" class="form-control" id="cc-expiration" placeholder="" required>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Expiration date required--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-md-3 mb-3">--}}
+                                {{--<label for="cc-expiration">CVV</label>--}}
+                                {{--<input type="text" class="form-control" id="cc-cvv" placeholder="" required>--}}
+                                {{--<div class="invalid-feedback">--}}
+                                    {{--Security code required--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<hr class="mb-4">--}}
                         <button class="btn btn-primary btn-lg btn-block" type="submit">پرداخت نهایی</button>
 
                     </form>
@@ -241,11 +223,11 @@
                         @foreach(Cart::content() as $item)
                             <li class="list-group-item d-flex justify-content-between lh-condensed">
                                 <div>
-                                    <h6 class="my-0">{{$item->model->name}}</h6>
+                                    {{--<h6 class="my-0">{{$item->model->name}}</h6>--}}
                                     <h6 class="my-0">{{$item->name}}</h6>
                                     {{-- <small class="text-muted">{{$item->model->details}}</small> --}}
                                 </div>
-                                <span class="text-muted">{{$item->model->price}}</span>
+                                {{--<span class="text-muted">{{$item->model->price}}</span>--}}
                                 <span class="text-muted">{{$item->price}}</span>
                             </li>
 
